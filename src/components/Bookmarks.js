366da802ff062a5af26f14df78bf817c32aa34dc
Sallot27 <sallotk12@gmail.com>
2023-12-07 21:14:33 +0300

    Commit 1

---
+++ src/components/Bookmarks.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Bookmarks = () => {
+  return (
+    <div>
+      <h1>My Bookmarks</h1>
+      <p>This is the bookmarks page of the app.</p>
+    </div>
+  );
+};
+
+export default Bookmarks;
